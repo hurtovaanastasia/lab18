069e4bad3f1b0bdf525b1cd39bceb444e9316d9c
Анастасия <hurtovanasta85@gmail.com>
2025-05-13 09:08:19 +0300

    ddd

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
-import java.util.Comparator;
+
 public class Main {
-    public static <T> void sortArray(T[] array, Comparator<T> comparator) {
-        Arrays.sort(array, comparator);
+    @FunctionalInterface
+    interface Functional{
+        boolean comp(int a, int b);
+    }
+    public static void insertionSort(int[] SortArr,Functional one){
+        for(int i=1; i<SortArr.length;i++){
+            int temp =SortArr[i];
+            int j;
+            for(j=i; j>0 && one.comp(temp, SortArr[j-1]); j--){
+                SortArr[j]=SortArr[j-1];
+            }
+            SortArr[j]=temp;
+        }
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите количество элементов: ");
         int size = input.nextInt();
-        Integer[] numbers = new Integer[size];
+        int[] sortir = new int[size];
         Random random = new Random();
         for (int i = 0; i < size; i++) {
-            numbers[i] = random.nextInt(100);
+            sortir[i] = random.nextInt(100);
         }
-        Comparator<Integer> ascendingOrder = (a, b) -> a.compareTo(b);
-        Comparator<Integer> descendingOrder = (a, b) -> b.compareTo(a);
-        System.out.println("Исходный массив: " + Arrays.toString(numbers));
-        sortArray(numbers, ascendingOrder);
-        System.out.println("По возрастанию: " + Arrays.toString(numbers));
-        sortArray(numbers, descendingOrder);
-        System.out.println("По убыванию:    " + Arrays.toString(numbers));
+        System.out.println("Введите 1 для сортировки по возрастанию, и 2 для сортировки по убыванию");
+        int select = input.nextInt();
+        if(select ==1){
+            insertionSort(sortir,(a,b) -> a < b);
+            System.out.println("Результат сортировки по возрастанию "+ Arrays.toString(sortir));
+        }
+        else if(select ==2){
+            insertionSort(sortir,(a,b) -> a > b );
+            System.out.println("Результат сортировки по убыванию "+ Arrays.toString(sortir));
+        }
+        else{
+            System.out.println("Такой операции нет");
+        }
     }
 }
